2ad3ff2a8f03ddc92e1e5b0412b35766148ac09c
Benjamin Bowles <ben@ceghirepro.uk>
2017-08-02 20:49:07 +0100

    Updates

--- src/CurrentAPI.php
+++ src/CurrentAPI.php
@@ -5,17 +5,28 @@ namespace Ceghirepro\Current;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Http\Request;
+
 use GuzzleHttp\Exception\GuzzleException;
 use GuzzleHttp\Client;
 
 class CurrentAPI
 {
+	protected $client;
+	
+	/**
+	*
+	**/
+	public function __construct()
+  {
+      $this->client = new Client;
+  }
+
 	/**
 	* Test function
 	* @return dd("hello")
 	*/
-	public function hello() {
-		$client = new Client();
-		dd($client);
+	public function hello()
+	{
+			dd($this->client);
 	}
 }
--- src/CurrentServiceProvider.php
+++ src/CurrentServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace Ceghirepro\Current;
 
+require_once __DIR__ . '/../vendor/autoload.php';
+
 use Illuminate\Support\ServiceProvider;
 
 class CurrentServiceProvider extends ServiceProvider
